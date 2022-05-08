6a8a3fd0136b9568532e60df33bb45774666f235
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2022-05-07 21:13:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express()
+
+
+// GET / HTTP/1.1
+app.get('/', (request, response) => {
+    response.send('Home Page! GET...')
+});
+
+// GET ALL
+app.get('/products/all', (request, response) => {
+    response.send('Products, '+ request.query.sort);
+});
+
+app.get('/products/:productId-:productSize-:productColor', (request, response)=>{
+    response.send('Products ' + request.params.productId+', '+ request.params.productSize+', '+ request.params.productColor );
+});
+
+app.get('/products/:productId', (request, response)=>{
+    response.send('Products' + request.params.productId);
+});
+
+app.listen(3000);
