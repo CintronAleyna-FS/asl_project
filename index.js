4a2115990f50e488d6b1ffefd46a5c9975e4fd35
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2022-05-29 16:43:08 -0500

    file upload express

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable file uploads
+const fileUpload = require('express-fileupload')
+app.use(fileUpload())
+
 const productRouter = require('./routes/Products');
 const variantsRouter = require('./routes/Variants');
 const imagesRouter = require('./routes/Images');
-
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'twig');
 
@@ -16,6 +20,7 @@ app.get('/', (req, res) =>{
 app.use("/products", productRouter)
 app.use("/variants", variantsRouter)
 app.use("/images", imagesRouter)
+
 app.use(express.static('public'))
 
 app.listen(3000);
